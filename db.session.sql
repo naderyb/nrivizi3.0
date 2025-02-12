417f20240c86b3d6d31b3f917699f1aad18bfc92
Nader youb <youb.nader@gmail.com>
2025-02-12 01:18:45 +0100

    changes

---
+++ db.session.sql
@@ -0,0 +1,26 @@
+CREATE TABLE submission (
+    id SERIAL PRIMARY KEY,
+    nomPrenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    formation VARCHAR(255) NOT NULL,
+    motivation TEXT NOT NULL,
+    submission_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    validated BOOLEAN DEFAULT FALSE,
+    validated_by VARCHAR(255),
+    deleted BOOLEAN DEFAULT FALSE,
+    deleted_by VARCHAR(255)
+);
+
+-- @block
+CREATE TABLE admins (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- @block
+INSERT into admins (username, password) VALUES 
+('yb_nader', '@nexus2005@'),
+('tb_seif', '@nexus2005@'),
+('bt_selena', '@nexus2005@'),
+('bd_hadil', '@nexus2005@');
